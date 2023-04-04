3261ed2f191b95983a124a7ca8b5573556912d25
Lella Misikir <lellamisikir@gmail.com>
2023-04-04 23:17:12 +0300

    MLC

---
+++ UnitOneOne/helloworld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - main prints hello world
+  * simple c programm
+  * Return: Returns 0
+  */
+int main(void)
+{
+	printf("Hello Lella aka World!");
+	return (0);
+}
